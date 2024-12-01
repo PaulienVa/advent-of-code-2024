567907828dc4f9c6f685706d81b63f3524e3ec14
Paulien van Alst <paulien@openvalue.nl>
2024-12-01 08:01:18 +0100

    [Day 1]: solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,28 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val columns = input.map { line -> line.split("   ")}
+        val sortedColumn1 = columns.map { it[0].toInt() }.sorted()
+        val sortedColumn2 = columns.map { it[1].toInt() }.sorted()
+
+        val sumOfDistances = sortedColumn1
+            .mapIndexed { index, el -> el to sortedColumn2[index] }
+            .sumOf { abs(it.first - it.second) }
+        return sumOfDistances
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val columns = input.map { line -> line.split("   ")}
+        val column1 = columns.map { it[0].toInt() }
+        val column2 = columns.map { it[1].toInt() }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return column1.map { left -> left to column2.count { left == it } }.sumOf { it.first * it.second }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
