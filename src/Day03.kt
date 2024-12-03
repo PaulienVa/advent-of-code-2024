758234cd8034d0e0f467e8f9337276590262bd51
Paulien van Alst <paulien@openvalue.nl>
2024-12-03 06:48:02 +0100

    Day3 : Part 1 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,7 +3,12 @@ import kotlin.math.abs
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return 0
+        val sumOfInstructions = input.map {
+            val groupValues = Regex("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)").findAll(it)
+            groupValues!!.map { it.destructured }.map { it.component1().toInt() to it.component2().toInt() }
+                .map { it.first * it.second }.sum()
+        }
+        return sumOfInstructions.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -14,8 +19,8 @@ fun main() {
     val testInput = readInput("Day03_test")
 
     println(part1(testInput))
-    check(part1(testInput) == 2)
-    check(part2(testInput) == 4)
+    check(part1(testInput) == 161)
+//    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
