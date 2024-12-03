e4d191923518b90a5b2c5b0d3db8e42ae7eed183
Paulien van Alst <paulien@openvalue.nl>
2024-12-03 12:58:35 +0100

    Day 3 - solution part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,17 +2,27 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun findInstructions(input: List<String>): List<Int> {
+        val regex = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
         val sumOfInstructions = input.map {
-            val groupValues = Regex("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)").findAll(it)
+            val groupValues = regex.findAll(it)
             groupValues!!.map { it.destructured }.map { it.component1().toInt() to it.component2().toInt() }
                 .map { it.first * it.second }.sum()
         }
-        return sumOfInstructions.sum()
+        return sumOfInstructions
     }
 
+    fun part1(input: List<String>): Int {
+        val sumOfInstructions = findInstructions(input)
+        return sumOfInstructions.sum()
+    }
     fun part2(input: List<String>): Int {
-        return 0
+        val toRemoveRegex = "don't\\(\\)(.*?)do\\(\\)|".toRegex()
+
+        val cleanedInput = toRemoveRegex.replace(input.joinToString(), "")
+        val sumOfInstructions = findInstructions(listOf(cleanedInput))
+        sumOfInstructions.println()
+        return sumOfInstructions.sum()
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -20,7 +30,7 @@ fun main() {
 
     println(part1(testInput))
     check(part1(testInput) == 161)
-//    check(part2(testInput) == 4)
+    check(part2(testInput) == 48)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
