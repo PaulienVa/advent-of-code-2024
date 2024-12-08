5c8c73d197b84e875b0e231462686d36a9af036b
Paulien van Alst <paulien@openvalue.nl>
2024-12-08 16:39:34 +0100

    Day 7: solution part 2

--- src/Day7.kt
+++ src/Day7.kt
@@ -3,16 +3,24 @@ import kotlin.math.abs
 
 fun main() {
     fun tryOperation(possibleOperations: List<(Long, Long) -> Long>, toVerify: Long, sum: Long, rest: List<Long>): Boolean {
-        if (sum == toVerify) {
-            return true
+        if (rest.isEmpty()) {
+            return sum == toVerify
         } else if (sum > toVerify) {
-            return false
-        } else if (rest.isEmpty()) {
             return false
         } else {
             return possibleOperations.any { operation -> tryOperation(possibleOperations, toVerify, operation.invoke(sum, rest[0]), rest.drop(1) ) }
         }
     }
+
+    fun findCalibrations(
+        input: List<String>,
+        possibleOperations: List<(Long, Long) -> Long>
+    ) = input.map { it.split(": ") }
+        .map { it[0].toLong() to it[1].split(" ").map { it.toLong() } }
+        .filter {
+            tryOperation(possibleOperations, it.first, it.second.first(), it.second.drop(1))
+        }.sumOf { it.first }
+
     fun part1(input: List<String>): Long {
         val addition: (Long, Long) -> Long = { x, y -> x + y }
         val multiplication: (Long, Long) -> Long = { x, y -> x * y }
@@ -20,16 +28,20 @@ fun main() {
             addition, multiplication
         )
 
-        val calibrations = input.map { it.split(": ") }
-            .map { it[0].toLong() to it[1].split(" ").map { it.toLong() } }
-            .filter {
-                tryOperation(possibleOperations, it.first, it.second.first(), it.second.drop(1))
-            }.sumOf { it.first }
+        val calibrations = findCalibrations(input, possibleOperations)
         return calibrations
     }
 
-    fun part2(input: List<String>): Int {
-      return 0
+    fun part2(input: List<String>): Long {
+        val addition: (Long, Long) -> Long = { x, y -> x + y }
+        val multiplication: (Long, Long) -> Long = { x, y -> x * y }
+        val concatenation : (Long, Long) -> Long = { x, y -> "$x$y".toLong() }
+        val possibleOperations = listOf(
+            addition, multiplication
+        )
+
+        val calibrations = findCalibrations(input,possibleOperations + concatenation)
+        return calibrations
     }
 
 
@@ -39,13 +51,13 @@ fun main() {
 //    println("Part 1 with testInput: ${part1(testInput)}")
 
     check(part1(testInput) == 3749L)
-//    check(part2(testInput ) == 6)
+    check(part2(testInput ) == 11387L)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day07")
     "Part 1 ".println()
     part1(input).println()
     "Part 2 ".println()
-    part2(input, ).println()
-
+    part2(input).println()
+    Long.MAX_VALUE.println()
 }
